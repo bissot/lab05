f327107c180e2a833d9cd784db310bedc7ef01d8
Trevor Bisson <bissot@rpi.edu>
2021-03-01 22:14:38 -0500

    a message

--- tutorial.cxx
+++ tutorial.cxx
@@ -2,10 +2,17 @@
 #include <cmath>
 #include <iostream>
 #include <string>
+
 #include "TutorialConfig.h"
+
+// should we include the MathFunctions header?
+#ifdef USE_MYMATH
+#  include "MathFunctions.h"
+#endif
+
 int main(int argc, char* argv[])
 {
-    if (argc < 2) {
+  if (argc < 2) {
     // report version
     std::cout << argv[0] << " Version " << Tutorial_VERSION_MAJOR << "."
               << Tutorial_VERSION_MINOR << std::endl;
@@ -16,10 +23,14 @@ int main(int argc, char* argv[])
   // convert input to double
   const double inputValue = std::stod(argv[1]);
 
-  // calculate square root
+  // which square root function should we use?
+#ifdef USE_MYMATH
+  const double outputValue = mysqrt(inputValue);
+#else
   const double outputValue = sqrt(inputValue);
+#endif
+
   std::cout << "The square root of " << inputValue << " is " << outputValue
             << std::endl;
   return 0;
 }
-
